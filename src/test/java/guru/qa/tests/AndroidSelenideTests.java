f711ac86e45adc2326c0df949c8812534729b864
Aleksandr Butakov <anbngm@gmail.com>
2022-05-18 16:35:04 +0500

    Working test

--- src/test/java/guru/qa/tests/AndroidSelenideTests.java
+++ src/test/java/guru/qa/tests/AndroidSelenideTests.java
@@ -3,6 +3,7 @@ package guru.qa.tests;
 import com.codeborne.selenide.Condition;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.MobileBy;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
@@ -14,18 +15,6 @@ public class AndroidSelenideTests extends TestBase {
 
     @Test
     void searchTest() {
-        /*
-        step("Type search", () -> {
-            // $(AppiumBy.accessibilityId("Search Wikipedia")).click();
-            $(AppiumBy.id("ru.anbn.testapplication:id/editText1")).setValue("12");
-
-            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text"))
-                    .setValue("BrowserStack");
-
-            $(AppiumBy.id("ru.anbn.testapplication:id/editText2")).setValue("5");
-            $(AppiumBy.id("ru.anbn.testapplication:id/button1")).click();
-        });
-         */
         step("Checking the opening of the main page", () -> {
             $(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/" +
                     "android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/" +
@@ -37,30 +26,72 @@ public class AndroidSelenideTests extends TestBase {
             $(AppiumBy.id("com.anbn.pinout:id/textView1")).shouldHave(text("Выберите интересующий тип"));
         });
 
+        step("Checking Main menu, Search", () -> {
+            $(AppiumBy.xpath("//android.widget.ImageView[@content-desc=\"Ещё\"]")).click();
+            $$(AppiumBy.id("com.anbn.pinout:id/title")).findBy(text("Строка")).click();
+            $(AppiumBy.id("com.anbn.pinout:id/textView1")).shouldHave(text("Для поиска необходимого оборудо"));
+            $(AppiumBy.id("android:id/search_button")).click();
+        });
+
+        step("Search 'SMK-30 MUX'", () -> {
+            $(AppiumBy.id("android:id/search_src_text")).setValue("СМК-30");
+            $$(AppiumBy.id("android:id/text1")).findBy(text("СМК-30 (MUX)")).click();
+            $(AppiumBy.id("com.anbn.pinout:id/textView")).shouldHave(text("СМК-30 (MUX)"));
+            $(AppiumBy.id("com.anbn.pinout:id/button1")).click();
+        });
+
+        step("Search 'CMA'", () -> {
+            $(AppiumBy.id("android:id/search_src_text")).setValue("СМА");
+            $$(AppiumBy.id("android:id/text1")).findBy(text("СМА-2-4")).click();
+            $(AppiumBy.id("com.anbn.pinout:id/textView")).shouldHave(text("СМА-2-4"));
+            $(AppiumBy.id("com.anbn.pinout:id/button1")).click();
+        });
+
+        step("Search 'MDK'", () -> {
+            $(AppiumBy.id("android:id/search_src_text")).setValue("МДК");
+            $$(AppiumBy.id("android:id/text1")).findBy(text("МДК-М1")).click();
+            $(AppiumBy.id("com.anbn.pinout:id/textView")).shouldHave(text("МДК-М1"));
+            $(AppiumBy.id("com.anbn.pinout:id/button1")).click();
+        });
+
+        step("Search 'ASR-920'", () -> {
+            $(AppiumBy.id("android:id/search_src_text")).setValue("ASR-920");
+            $$(AppiumBy.id("android:id/text1")).findBy(text("ASR-920")).click();
+            $(AppiumBy.id("com.anbn.pinout:id/textView")).shouldHave(text("ASR-920"));
+            $(AppiumBy.id("com.anbn.pinout:id/button1")).click();
+            $(AppiumBy.id("com.anbn.pinout:id/button")).click();
+        });
+
         step("Checking Main menu, About", () -> {
             $(AppiumBy.xpath("//android.widget.ImageView[@content-desc=\"Ещё\"]")).click();
-            $(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/" +
-                    "android.widget.ListView/android.widget.LinearLayout[2]/android.widget.LinearLayout/" +
-                    "android.widget.RelativeLayout/android.widget.TextView")).click();
+            $$(AppiumBy.id("com.anbn.pinout:id/title")).findBy(text("О программе")).click();
             $(AppiumBy.id("com.anbn.pinout:id/textView3")).shouldHave(text("О ПРОГРАММЕ"));
             $(AppiumBy.id("com.anbn.pinout:id/button1")).click();
             $(AppiumBy.id("com.anbn.pinout:id/textView1")).shouldHave(text("Выберите интересующий тип"));
         });
 
-        step("Checking Main menu, Search", () -> {
-            $(AppiumBy.xpath("//android.widget.ImageView[@content-desc=\"Ещё\"]")).click();
-
-            $(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/" +
-                    "android.widget.ListView/android.widget.LinearLayout[1]/android.widget.LinearLayout/" +
-                    "android.widget.RelativeLayout/android.widget.TextView")).click();
-            $(AppiumBy.id("com.anbn.pinout:id/textView1")).shouldHave(text("Для поиска необходимого оборудо"));
-            $(AppiumBy.id("android:id/search_button")).click();
-            $(AppiumBy.id("android:id/search_src_text")).setValue("asr");
-            $$(AppiumBy.id("android:id/text1")).findBy(text("CWDM")).click();
-
+        step("Manual selection of equipment Cisco", () -> {
+            $(AppiumBy.id("com.anbn.pinout:id/spinner1")).click();
+            $$(AppiumBy.id("android:id/text1")).findBy(text("Оборудование Cisco, ВСТСПД")).click();
+            $(AppiumBy.id("com.anbn.pinout:id/spinner2")).click();
+            $$(AppiumBy.id("android:id/text1")).findBy(text("RJ-45 connector Ethernet")).click();
+            $(AppiumBy.id("com.anbn.pinout:id/button1")).click();
+            $(AppiumBy.id("com.anbn.pinout:id/textView")).shouldHave(text("RJ-45 connector Ethernet"));
+            $(AppiumBy.id("com.anbn.pinout:id/button1")).click();
         });
 
-        sleepMethod();
+        step("Manual selection of equipment VSTSPD", () -> {
+            $(AppiumBy.id("com.anbn.pinout:id/spinner1")).click();
+            $$(AppiumBy.id("android:id/text1")).findBy(text("Оборудование Cisco, ВСТСПД")).click();
+            $(AppiumBy.id("com.anbn.pinout:id/spinner2")).click();
+            $$(AppiumBy.id("android:id/text1")).findBy(text("Оборудование ВСТСПД")).click();
+            $(AppiumBy.id("com.anbn.pinout:id/button1")).click();
+            $(AppiumBy.id("com.anbn.pinout:id/textView")).shouldHave(text("Оборудование ВСТСПД"));
+            $(AppiumBy.id("com.anbn.pinout:id/button1")).click();
+        });
+    }
+
+}
 
         /*
         step("Verify content found", () ->
@@ -73,10 +104,15 @@ public class AndroidSelenideTests extends TestBase {
         );
          */
 
-    }
+        /*
+        step("Type search", () -> {
+            // $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+            $(AppiumBy.id("ru.anbn.testapplication:id/editText1")).setValue("12");
 
-    void sleepMethod() {
-        sleep(1000);
-    }
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text"))
+                    .setValue("BrowserStack");
 
-}
+            $(AppiumBy.id("ru.anbn.testapplication:id/editText2")).setValue("5");
+            $(AppiumBy.id("ru.anbn.testapplication:id/button1")).click();
+        });
+         */
